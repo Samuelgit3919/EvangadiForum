2a2fdb6dbde1a6fed5b409a3fbf2bc519419d527
Nehu4jesus <enehemiya@yahoo.com>
2025-03-20 10:43:55 +0300

    footer link

--- front-end/src/components/Footer/Footer.jsx
+++ front-end/src/components/Footer/Footer.jsx
@@ -8,15 +8,37 @@ function Footer() {
   return (
     <footer className={Style.footer_container}>
       <div className={Style.footer_section}>
-        <img src="/logo2.png" alt="Evangadi Logo" className={Style.footer_logo} />
+        <img
+          src="/logo2.png"
+          alt="Evangadi Logo"
+          className={Style.footer_logo}
+        />
         <div className={Style.social_icons}>
-          <FacebookOutlinedIcon className={Style.icon} />
-          <InstagramIcon className={Style.icon} />
-          <YouTubeIcon className={Style.icon} />
+          <a
+            href="https://facebook.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FacebookOutlinedIcon className={Style.icon} />
+          </a>
+          <a
+            href="https://instagram.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <InstagramIcon className={Style.icon} />
+          </a>
+          <a
+            href="https://youtube.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <YouTubeIcon className={Style.icon} />
+          </a>
         </div>
       </div>
       <div className={Style.footer_section}>
-        <h3 >Useful Link</h3>
+        <h3>Useful Link</h3>
         <a href="#">How it works</a>
         <a href="#">Terms of Service</a>
         <a href="#">Privacy policy</a>
